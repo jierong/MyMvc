e3f6dd6047128cc5f79d1cfeea1a4c969b8041c7
Jie RONG <jierong@microsoft.com>
2019-11-14 10:18:05 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -57,6 +57,7 @@ namespace MyMvc
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                
             });
         }
     }
